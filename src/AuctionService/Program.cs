943ad10afd2e1ef310bd5ea734b06b2253e7a968
Andyrub18 <andersonruban1281@gmail.com>
2023-10-17 15:42:45 -0400

    end of section 4

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,4 +1,6 @@
+using AuctionService.Consumers;
 using AuctionService.Data;
+using MassTransit;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,11 +8,37 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<AuctionDbContext>(opt =>
-{
-  opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+builder.Services.AddDbContext<AuctionDbContext>(
+  opt =>
+    opt.UseNpgsql(
+      builder.Configuration.GetConnectionString("DefaultConnection")
+    )
+);
+
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddMassTransit(x =>
+{
+  x.AddEntityFrameworkOutbox<AuctionDbContext>(o =>
+  {
+    o.QueryDelay = TimeSpan.FromSeconds(10);
+
+    o.UsePostgres();
+    o.UseBusOutbox();
+  });
+
+  x.AddConsumersFromNamespaceContaining<AuctionCreatedFaultConsumer>();
+
+  x.SetEndpointNameFormatter(
+    new KebabCaseEndpointNameFormatter("auction", false)
+  );
+
+  x.UsingRabbitMq((context, cfg) => cfg.ConfigureEndpoints(context));
+});
+
+builder.Services
+  .AddOptions<RabbitMqTransportOptions>()
+  .Configure(options => options.Port = 5673);
 
 var app = builder.Build();
 
